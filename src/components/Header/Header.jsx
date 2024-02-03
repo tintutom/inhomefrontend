9e0618aea1a23bcb9501df3ada8b0b2f78577362
Tintu Tom <81849852+tintutom@users.noreply.github.com>
2024-02-03 09:02:38 +0530

    resolve data updating through refresh

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,99 +1,3 @@
-// import React from 'react'
-// import logo from '../../assets/images/inhome-consulting-logo11.jpg';
-// import userImg from '../../assets/images/avatar-icon.png'
-// import { NavLink,Link } from 'react-router-dom';
-// import { useEffect,useRef } from 'react';
-// import {BiMenu} from 'react-icons/bi';
-// const navLinks=[
-//     {
-//         path:'/home',
-//         display:'Home'
-//     },
-//     {
-//         path:'/doctors',
-//         display:'Find a Doctor'
-//     },
-//     {
-//         path:'/services',
-//         display:'Services'
-//     },
-//     {
-//         path:'/contact',
-//         display:'Contact'
-//     }
-// ]
-// const Header = () => {
-
-//     const headerRef = useRef(null)
-//     const menuRef = useRef(null)
-
-//     const handleStickyHeader =()=>{
-//         window.addEventListener('scroll',()=>{
-//             if(document.body.scrollTop>80 || document.documentElement.scrollTop > 80){
-//                 headerRef.current.classList.add('sticky__header')
-//             }else{
-//                 headerRef.current.classList.remove('sticky__header')
-//             }
-//         })
-//     }
-//     useEffect(()=>{
-//         handleStickyHeader()
-//         return ()=>window.removeEventListener('scroll',handleStickyHeader)
-//     })
-//     const toggleMenu=()=>menuRef.current.classList.toggle('show__menu')
-//   return (
-//     <header className='header flex items-center'ref={headerRef}>
-//         <div className='container'>
-//             <div className='flex items-center justify-between'>
-//                 {/*===========logo========*/}
-//                 <div>
-//                     <img src={logo} alt=''/>
-//                 </div>
-//             {/*===========menu========*/}
-//             <div className='navigation' ref={menuRef} onClick={toggleMenu}>
-//                 <ul className='menu flex items-center gap-[2.7rem]'>
-//                     {navLinks.map((link,index)=>(
-//                         <li key={index}>
-//                             <NavLink to={link.path} 
-//                                 className={navclass=>
-//                                     navclass.isActive 
-//                                         ? 'text-primaryColor text-[16px] leading-7 font-[600]'
-//                                         :'text-textColor textp-[16px] leading-7 font-[500] hover:text-primaryColor'
-//                                 }
-//                             >
-//                                 {link.display}
-//                             </NavLink>
-//                         </li>
-//                         )
-//                     )}
-//                 </ul>
-//             </div>
-//             {/* ==========nav right=================*/}
-//             <div className='flex items-center gap-4'>
-//                 <div className='hidden'>
-//                     <Link to='/'>
-//                         <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
-//                             <img src={userImg} className='w-full rounded-full' alt='' />
-//                         </figure>
-//                     </Link>
-//                 </div>
-
-//             <Link to='/login'>
-//                 <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center
-//                 justify-center rounded-[50px]'>Login</button>
-//             </Link>
-//             <span className='md:hidden' onClick={toggleMenu}>
-//                 <BiMenu className='w-6 h-6 cursor-pointer'/>
-//             </span>
-//             </div>
-//             </div>
-//         </div>
-        
-//     </header>
-//   )
-// }
-
-// export default Header
 
 import { NavDropdown } from 'react-bootstrap';
 import logo from '../../assets/images/logo.png';
@@ -150,37 +54,23 @@ const navLinksLogin = [
 const Header = () => {
   const headerRef = useRef(null);
   const menuRef = useRef(null);
+  const token = Cookies.get('jwt_user');
   const [showProfileDropdown, setShowProfileDropdown] = useState(false); // Added state for showing/hiding profile dropdown
   const [userImage,setUserImage]=useState(null);
+  const [isHeaderSticky, setIsHeaderSticky] = useState(false);
+  const [count,setCount]=useState([])
+  const [userLogin,setUserLogin]=useState (null |token)
+  
+
 
   const [isHeaderWhite, setIsHeaderWhite] = useState(false);
-
   const handleStickyHeader = () => {
-    window.addEventListener('scroll', () => {
-      if (
-        document.body.scrollTop > 80 ||
-        document.documentElement.scrollTop > 80
-    //   ) {
-    //     headerRef.current.classList.add('sticky__header');
-    //   } else {
-    //     headerRef.current.classList.remove('sticky__header');
-    //   }
-    ) {
-        headerRef.current.style.position = 'fixed';
-        // headerRef.current.style.top = '0';
-        headerRef.current.style.width = '100%';
-        headerRef.current.style.zIndex = '1000'; 
-        setIsHeaderWhite(true);
-      } 
-      else {
-        headerRef.current.style.position = 'static';
-        setIsHeaderWhite(false);
-      }
-    });
+    const scrollPosition = window.scrollY;
+    setIsHeaderSticky(scrollPosition > 80);
   };
 
   useEffect(() => {
-    handleStickyHeader();
+    window.addEventListener('scroll', handleStickyHeader);
     return () => window.removeEventListener('scroll', handleStickyHeader);
   }, []);
 
@@ -196,44 +86,58 @@ const Header = () => {
     Cookies.remove('role', 'user');
     Cookies.remove('id');
     dispatch(logout());
+    dispatch(change(''));
+    
     navigate('/');
   };
 
   useEffect(() => {
-    const token = Cookies.get('jwt_user');
-    if (token) {
-      axios
-        .post(
-          verifyToken,
-          { token },
-          {
-            headers: { 'Content-Type': 'application/json' },
-          }
-        )
-        
-        .then((res) => {
+    const fetchData = async () => {
+      try {
+        const token = Cookies.get('jwt_user');
+        setUserLogin(token)
+        if (token ) {
+          const res = await axios.post(
+            verifyToken,
+            { token },
+            {
+              headers: { 'Content-Type': 'application/json' },
+            }
+          );
+          const userResponse = await axios.get(`${baseUrl}get-user/${res.data.id}`);
+          const userImagePath = `${userResponse.data.userimage}`;
+          setUserImage(userImagePath);
           dispatch(change(res.data.username));
-          
-          axios.get(`${baseUrl}get-user/${res.data.id}`)
-            .then((userResponse) => {
-              const userImagePath = `${userResponse.data.userimage}`;
-              setUserImage(userImagePath);
-            })
-            .catch((userError) => {
-              console.log('Error fetching user information:', userError);
-            });
-        })
-        .catch((err) => {
-          console.log('Error:', err);
-        });
-    }
-  }, []);
+          setCount((prevCount)=>prevCount+1)
+        }
+      } catch (error) {
+        console.log('Error:', error);
+      }
+    };
+    fetchData();
+  }, [token]); 
 
+  // useEffect(()=>{
+  //   LoadImage()
+  // },[token])
 
+  // const LoadImage = async()=>{
+  //   const token = Cookies.get('jwt_user');
+  //   const res = await axios.post(
+  //     verifyToken,
+  //     { token },
+  //     {
+  //       headers: { 'Content-Type': 'application/json' },
+  //     }
+  //   );
+  //   const userResponse = await axios.get(`${baseUrl}get-user/${res.data.id}`);
+  //   const userImagePath = `${userResponse.data.userimage}`;
+  //   setUserImage(userImagePath)
+
+  // }
   const toggleProfile = () => {
     navigate("/profile")
   };
-
   return (
     <header className="header flex items-center" ref={headerRef} style={{ width: '100%',paddingTop: '20px' }}>
       <div className="container">
@@ -295,15 +199,8 @@ const Header = () => {
                     alt=""
                     onClick={toggleProfile}
                   />
-                )}
-                  
-                  
+                )}                 
               </div>
-              {/* <Link to="/">
-                <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
-                  <img src={userImg} className="w-full rounded-full" alt="" />
-                </figure>
-              </Link> */}
             </div>
             {username ? (
               <div>
